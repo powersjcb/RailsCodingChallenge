3bdff68b5ba50fc46c28deed332d3d99eceab0a0
Jacob Powers <powersjcb@gmail.com>
2015-07-24 23:46:55 -0700

    Finish insersects? with specs

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,6 +1,16 @@
 
 class Cuboid
 
+  UNIT_VERTICES =
+       [[ 1,  1,  1],
+        [-1,  1,  1],
+        [-1, -1,  1],
+        [-1, -1, -1],
+        [ 1, -1,  1],
+        [ 1, -1, -1],
+        [ 1,  1, -1],
+        [-1,  1, -1]]
+
   attr_reader :x, :y, :z, :height, :width, :length
 
   #BEGIN public methods that should be your starting point
@@ -21,18 +31,59 @@ class Cuboid
 
   def move_to!(x, y, z)
     @x, @y, @z, = x, y, z
+
+    self
   end
 
-  # assumes that origin is at centroid
+  # assumes origin at centroid
   def vertices
-    unit_corners = []
+    vertices = UNIT_VERTICES.map do |vertex|
+      [
+        x + vertex[0] * @width  / 2,
+        y + vertex[1] * @height / 2,
+        z + vertex[2] * @length / 2
+      ]
+    end
 
-
+    vertices
   end
 
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
+    min_x < other.max_x &&
+    max_x > other.min_x &&
+    min_y < other.max_y &&
+    max_y > other.min_y &&
+    min_z < other.max_z &&
+    max_z > other.min_z
   end
 
-  #END public methods that should be your starting point
+
+  # public utility methods
+  def max_x
+    vertices.map { |vertex| vertex[0] }.max
+  end
+
+  def min_x
+    vertices.map { |vertex| vertex[0] }.min
+  end
+
+  def max_y
+    vertices.map { |vertex| vertex[1] }.max
+  end
+
+  def min_y
+    vertices.map { |vertex| vertex[1] }.min
+  end
+
+  def max_z
+    vertices.map { |vertex| vertex[2] }.max
+  end
+
+  def min_z
+    vertices.map { |vertex| vertex[2] }.min
+  end
+
+
+
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -7,8 +7,8 @@ describe Cuboid do
   subject(:subject) { Cuboid }
 
   context '#initialize' do
-    let(:d_cube) {subject.new}
-    let(:c_cube) {subject.new({ height: 2, width: 3, length: 4,
+    let(:d_cube) { subject.new }
+    let(:c_cube) { subject.new({ height: 2, width: 3, length: 4,
                                 x: 1.5, y: 2.5, z: 3.5 }) }
     it 'returns self' do
       expect(d_cube.class).to eq(subject)
@@ -29,16 +29,55 @@ describe Cuboid do
     it 'creates a cuboid with custom size' do
       expect([c_cube.height, c_cube.width, c_cube.length]).to eq([2,3,4])
     end
-
   end
 
-  context "#move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+  context '#vertices' do
+    let(:cube) { subject.new }
+
+    it 'provides correct number of vertices' do
+      expect(cube.vertices.length).to eq(8)
+    end
+
+    it 'contains all Numeric position values' do
+      expect(
+        cube.vertices.all? {|v| v.all? {|val| val.is_a? Numeric } }
+      ).to be true
     end
   end
 
-  context "intersects?" do
+  context "#move_to" do
+    let(:cube) { subject.new }
+    it "changes the origin in the simple happy case" do
+      cube.move_to!(1,2,3)
+      expect([cube.x, cube.y, cube.z]).to eq([1,2,3])
+    end
+  end
+
+  context "#intersects?" do
+    let(:cube_base)  { subject.new }
+    let(:cube_encap) { subject.new({ height: 0.5, width: 0.5, length: 0.5 }) }
+    let(:cube_no_overlap) {subject.new({ x: 10 })}
+    let(:cube_overlap) {subject.new({ x: 0.25 })}
+
+    it 'returns true when compared to itself' do
+      expect(cube_base.intersects?(cube_base)).to be true
+    end
+
+    it 'returns true when compared to an encapsulated cube' do
+      expect(cube_base.intersects?(cube_encap)).to be true
+    end
+
+    it 'returns true when encapsulated by another cube' do
+      expect(cube_encap.intersects?(cube_base)).to be true
+    end
+
+    it 'returns true when partially overlapping' do
+      expect(cube_encap.intersects?(cube_overlap)).to be true
+    end
+
+    it 'returns false when not overlapping' do
+      expect(cube_base.intersects?(cube_no_overlap)).to be false
+    end
   end
 
 end
