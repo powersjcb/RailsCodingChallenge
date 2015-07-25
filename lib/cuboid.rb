524ef9931062b52ca8d8359a580edb0a71658fe0
Jacob Powers <powersjcb@gmail.com>
2015-07-24 21:23:29 -0700

    Add some tests, think about best way to handle verticies

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,38 @@
 
 class Cuboid
-  
+
+  attr_reader :x, :y, :z, :height, :width, :length
+
   #BEGIN public methods that should be your starting point
+  def initialize(options = {})
+    defaults = { height: 1, width: 1, length: 1, x: 0, y: 0, z: 0 }
+    options = defaults.merge(options)
+
+    @x = options[:x]
+    @y = options[:y]
+    @z = options[:z]
+
+    @width = options[:width]   # x axis
+    @height = options[:height] # y axis
+    @length = options[:length] # z axis
+
+    self
+  end
 
   def move_to!(x, y, z)
+    @x, @y, @z, = x, y, z
   end
-  
+
+  # assumes that origin is at centroid
   def vertices
+    unit_corners = []
+
+
   end
-  
+
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
   end
 
-  #END public methods that should be your starting point  
+  #END public methods that should be your starting point
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,44 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
+  subject(:subject) { Cuboid }
+
+  context '#initialize' do
+    let(:d_cube) {subject.new}
+    let(:c_cube) {subject.new({ height: 2, width: 3, length: 4,
+                                x: 1.5, y: 2.5, z: 3.5 }) }
+    it 'returns self' do
+      expect(d_cube.class).to eq(subject)
+    end
+
+    it 'creates a cuboid with default centroid at origin' do
+      expect([d_cube.x, d_cube.y, d_cube.z]).to eq([0,0,0])
+    end
+
+    it 'creates a cuboid with default edge length of 1' do
+      expect([d_cube.height, d_cube.width, d_cube.length]).to eq([1,1,1])
+    end
+
+    it 'creates a cuboid with custom position' do
+      expect([c_cube.x, c_cube.y, c_cube.z]).to eq([1.5, 2.5, 3.5])
+    end
+
+    it 'creates a cuboid with custom size' do
+      expect([c_cube.height, c_cube.width, c_cube.length]).to eq([2,3,4])
+    end
+
+  end
+
+  context "#move_to" do
     it "changes the origin in the simple happy case" do
       expect(subject.move_to!(1,2,3)).to be true
     end
-  end    
-  
-  describe "intersects?" do
+  end
+
+  context "intersects?" do
   end
 
 end
